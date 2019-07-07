edc155f3c9e1d0276c39fc4a5d472400ba2c1771
loisklee <lois.lee@me.com>
2019-07-07 14:53:30 -0400

    set up routing for home page and about page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :weeks
-    has_many :practices, through weeks
+    has_many :practices, through: :weeks
     has_many :custom_practices, class_name: "Practice", foreign_key: "creator_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :weeks
   resources :practices
   resources :users
+
+  root to: 'welcome#home'
+  get '/about', to: 'welcome#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
