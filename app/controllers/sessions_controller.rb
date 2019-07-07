10c705e7c7f3af6b7d90441f2f3c2de54022bd6a
loisklee <lois.lee@me.com>
2019-07-07 14:59:47 -0400

    create route and view template for signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
-
 class SessionsController < ApplicationController
 
     def new
     end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+    private
+    def login
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root to: 'welcome#home'
   get '/about', to: 'welcome#about'
+  get '/login', :to => 'sessions#new', :as => :login
+  get '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
