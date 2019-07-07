1f3520627c1469189ef393fb0887b6cb557d3bda
loisklee <lois.lee@me.com>
2019-07-07 14:48:25 -0400

    setup model relations

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,8 @@
 class Practice < ApplicationRecord
+    belongs_to :user
+    belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+    
+    has_many :plans
+    has_many :weeks, through: :plans
+
 end
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,2 +1,6 @@
 class Week < ApplicationRecord
+    belongs_to :user
+
+    has_many :plans
+    has_many :practices, through: :plans
 end
