7f355fae7448a1f00b874f34101fd24120b5183b
loisklee <lois.lee@me.com>
2019-07-07 22:55:39 -0400

    update weeks controller and add new week form

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,8 +1,47 @@
 class WeeksController < ApplicationController
-    before_action :require_login
+  before_action :require_login
 
-    def new
-      verify_user
-      @week = current_user.weeks.new
+  def new
+    verify_user
+    @week = current_user.weeks.new
+  end
+
+  def create
+    @week = Week.new(week_params)
+    if @week.save
+      current_user.weeks << @week
+      redirect_to user_path(current_user)
+    else
+      render 'new'
     end
+  end
+
+  def edit
+    verify_user
+    @week = current_user.weeks.find_by(id: params[:id])
+    redirect_to user_path(current_user) if @week.nil? || !verify(@week)
+  end
+
+  def update
+    set_week
+    @week.update(week_params)
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    set_week
+    @week.plans.destroy_all
+    @week.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+  
+  def week_params
+    params.require(:week).permit(:start_date, :user_id, :completed, practice_ids:[])
+  end
+
+  def verify_user
+    redirect_to user_path(current_user) if !User.exists?(params[:user_id]) || current_user != User.find(params[:user_id])
+  end
 end
