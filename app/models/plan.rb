02938210d5f2e23158ac5ac02957a453cd6238bb
loisklee <lois.lee@me.com>
2019-07-07 23:15:35 -0400

    enforce new week start date

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,10 @@
 class Plan < ApplicationRecord
     belongs_to :week
     belongs_to :practice
+
+    accepts_nested_attributes_for :week, reject_if: :reject_week
+  
+    def reject_week(attributes)
+      attributes['start_date'].blank?
+    end
 end
