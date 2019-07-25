df99da17aa2fde6c6e42ded0e3895944e5287d2c
Lois Lee <lois.lee@me.com>
2019-07-25 10:57:06 -0400

    set post request, update tips controller to respond to format

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,54 +1,65 @@
+    
 class TipsController < ApplicationController
-    before_action :require_login
-  
-    def new
-      if Practice.exists?(params[:practice_id])
-        @practice = Practice.find_by(id: params[:practice_id])
-        @tip = @practice.tips.build(user_id: current_user.id)
-      else
-        redirect_to practices_path
-      end
-    end
-  
-    def create
-      @tip = Tip.new(tip_params)
-      if @tip.save
-        redirect_to practice_path(@tip.practice)
-      else
-        render 'new'
-      end
-    end
-  
-    def edit
+  before_action :require_login, :set_tip
+  skip_before_action :set_tip, :only => [:index, :new, :create, :edit]
+
+  def new
+    if Practice.exists?(params[:practice_id])
       @practice = Practice.find_by(id: params[:practice_id])
-      if @practice.nil?
-        redirect_to practices_path
-      else
-        @tip = @practice.tips.find_by(id: params[:id])
-        redirect_to practice_path(@practice) if @tip.nil?
-      end
+      @tip = @practice.tips.build(user_id: current_user.id)
+    else
+      redirect_to practices_path
     end
-  
-    def update
-      @tip = Tip.find_by(id: params[:id])
-      @tip.update(tip_params)
-      redirect_to practice_path(@tip.practice)
+  end
+
+  def create
+    tip = Tip.new(tip_params)
+    if tip.save
+      render json: tip, status: 201
+    else
+      render 'new'
     end
-  
-    def destroy
-      @tip = Tip.find_by(id: params[:id])
-      @tip.destroy
-      redirect_to practice_path(@tip.practice)    
+  end
+
+  def edit
+    @practice = Practice.find_by(id: params[:practice_id])
+    if @practice.nil?
+      redirect_to practices_path
+    else
+      @tip = @practice.tips.find_by(id: params[:id])
+      redirect_to practice_path(@practice) if @tip.nil? || !verify(@tip)
     end
+  end
+
+  def update
+    @tip.update(tip_params)
+    render json: @tip, status: 201
+  end
+
+  def like
+    @tip.like
+    render json: tip, status: 201
+    my_path
+  end
+
+  def unlike
+    @tip.unlike
+    my_path
+  end
+
+  def destroy
+    @tip = Tip.find_by(id: params[:id])
+    @tip.destroy
+    redirect_to practice_path(@tip.practice)    
+  end
   
-    def like
-      tip = Tip.find_by(id: params[:id])
-      tip.like
-      redirect_to practice_path(tip.practice)
+  private
+    def tip_params
+      params.require(:tip).permit(:user_id, :practice_id, :content, :likes)
     end
 
-    private
-      def tip_params
-        params.require(:tip).permit(:user_id, :practice_id, :content)
-      end
-  end
+    def my_path
+      set_tip
+      redirect_to practice_path(@tip.practice)
+    end
+end
